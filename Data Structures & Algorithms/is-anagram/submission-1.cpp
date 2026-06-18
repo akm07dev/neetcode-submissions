5f0798409bb2ed5c9bfa44834d39ea4dccebb8f2
Ankit K Megotia <64592422+akm07dev@users.noreply.github.com>
2026-06-18 23:30:41 +0530

    Bulk sync: 5 submissions

---
+++ Data Structures & Algorithms/is-anagram/submission-1.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        
+        if (s.length() != t.length()) return false;
+        vector<int> mp(26, 0);
+        for (int i = 0;  i<s.length() ; i++)
+        {
+            mp[s[i] - 'a'] ++;
+            mp[t[i] - 'a'] --;
+        }
+
+        for (auto it: mp) if (it != 0) return false;
+
+        return true; 
+    }
+};
---
+++ Data Structures & Algorithms/two-integer-sum/submission-0.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        
+        unordered_map<int, int> mp;
+        int n  = nums.size();
+        for(int i = 0; i<n; i++)
+        {
+            if (mp.contains(target - nums[i]))
+            {
+                return vector<int> {mp[target - nums[i]],  i};
+            } 
+            else
+                mp[nums[i]] = i;
+        }
+
+
+    }
+};
